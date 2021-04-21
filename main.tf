87a54f296550de057109b97aeabdcab63b7f0512
Nicolae Necula <Nicolae.Necula@outlook.com>
2021-04-21 11:29:25 +0200

    Added MainTF file

---
+++ main.tf
@@ -0,0 +1,163 @@
+#Defining provider
+
+provider "azurerm" {
+  features {}
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.52.0"
+    }
+  }
+
+}
+
+#Create Resource Group
+
+resource "azurerm_resource_group" "rg" {
+  name     = "PracticeRG"
+  location = "northeurope"
+}
+
+#Create Virtual Network
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "nnecula_vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    "ENV" = "Azure DevOps"
+  }
+
+
+}
+#Create Subnet
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "nnecula_test_subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "public_ip" {
+  name                = "nnecula_PIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+# Create Network Security Group and rule
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nnecula_NSG"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    ENV = "Azure DevOps"
+  }
+}
+
+# Create network interface
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nnecula_NIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "nnecula_NIC"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+
+  tags = {
+    ENV = "Azure DevOps"
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Create storage account for boot diag
+resource "azurerm_storage_account" "storage" {
+  name                     = "nneculalinuxdiag"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    ENV = "Azure DevOps"
+  }
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "linuxvm" {
+  name                  = "nneculanlinuxvm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name                   = "nneculanlinuxvm"
+  admin_username                  = "nnecula"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
+  }
+
+  tags = {
+    ENV = "Azure DevOps"
+  }
+}
